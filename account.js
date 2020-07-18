56a949e6e5f366a0f54c969c2aa8eefe771598de
Ujoy7851 <joy7851@gmail.com>
2020-07-18 21:53:43 +0900

    파일 복사, 삭제 및 가계부 기능 추가

---
+++ account.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+const { program } = require('commander');
+const inquirer = require('inquirer');
+
+const { version } = require('./package');
+const { sequelize, Account } = require('./models')
+
+program
+    .version(version, '-v, --version')
+    .name('account');
+
+program
+    .command('revenue <money> <desc>')
+    .description('수입 기록')
+    .action(async(money, desc) => {
+        await sequelize.sync();
+        await Account.create({
+            money: parseInt(money, 10),
+            desc,
+            type: true
+        });
+        await sequelize.close();
+    });
+
+program 
+    .command('expense <money> <desc>')
+    .description('지출 기록')
+    .action(async (money, desc) => {
+        await sequelize.sync();
+        await Account.create({
+            money: parseInt(money, 10),
+            desc,
+            type: false
+        });
+        await sequelize.close();
+    });
+
+program
+    .command('balance')
+    .description('잔액 기록')
+    .action(async() => {
+        await sequelize.sync();
+        const logs = await Account.findAll({});
+        const revenue = logs.filter(l => l.type === true).reduce((acc, cur) => acc + cur.money, 0);
+        const expense = logs.filter(l => l.type === false).reduce((acc, cur) => acc + cur.money, 0);
+        console.log(`${revenue - expense}원 남았습니다.`);
+        await sequelize.close();
+    });
+
+
+program
+    .action((cmd, args) => {
+        if(args) {
+            console.log('해당 명령어를 찾을 수 없습니다.');
+            program.help();
+        } else {
+            inquirer.prompt([{
+                type: 'list',
+                name: 'type',
+                message: '분류를 선택하세요.',
+                choices: ['수입', '지출', '잔액'],
+            }, {
+                type: 'input',
+                name: 'money',
+                message: '금액을 입력하세요.',
+                default: '0',
+                when: answer => answer.type === '수입' || '지출'
+            }, {
+                type: 'input',
+                name: 'desc',
+                message: '상세 내역을 작성하세요.',
+                defalut: '.',
+                when: answer => answer.type === '수입' || '지출'
+            }, {
+                type: 'confirm',
+                name: 'confirm',
+                message: '생성하시겠습니까?',
+            }])
+                .then(async (answer) => {
+                    if(answer.confirm) {
+                        if (answer.type === '수입') {
+                            await sequelize.sync();
+                            await Account.create({
+                                money: parseInt(answer.money, 10),
+                                desc,
+                                type: true
+                            });
+                            await sequelize.close();
+                        } else if (answer.type === '지출') {
+                            await sequelize.sync();
+                            await Account.create({
+                                money: parseInt(answer.money, 10),
+                                desc,
+                                type: false
+                            });
+                            await sequelize.close();
+                        } else {
+                            await sequelize.sync();
+                            const logs = await Account.findAll({});
+                            const revenue = logs.filter(l => l.type === true).reduce((acc, cur) => acc + cur.money, 0);
+                            const expense = logs.filter(l => l.type === false).reduce((acc, cur) => acc + cur.money, 0);
+                            console.log(`${revenue - expense}원 남았습니다.`);
+                            await sequelize.close();
+                        }
+                    }
+                });
+        }
+    })
+    .parse(process.argv);
--- template.js
+++ template.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { program } = require('commander');
 const inquirer = require('inquirer');
-const chalk = requite('chalk');
+const chalk = require('chalk');
 
 // let type = process.agrv[2];
 // let name = process.argv[3];
@@ -77,6 +77,35 @@ const makeTemplate = () => {
     }
 };
 
+const copyFile = (name, directory) => {
+    if(exist(name)) {
+        mkdirp(directory);
+        fs.copyFileSync(name, path.join(directory, name));
+        console.log(`${name}이 복사되었습니다.`)
+    } else {
+        console.error('파일이 존재하지 않습니다.');
+    }
+}
+
+const removeFile = (path) => {
+    if(exist(path)) {
+        try{
+            const dir = fs.readdirSync();
+            dir.forEach((d) => {
+                removeFile(path.join(path, d));
+            });
+            fs.rmdirSync(path);
+            console.log(`${path} 폴더를 삭제했습니다.`);
+        } catch(e) {
+            fs.unlinkSync(path);
+            console.log(`${path} 파일을 삭제했습니다.`);
+        }
+    } else {
+        console.error('파일 또는 폴더가 존재하지 않습니다.');
+    }
+}
+
+
 program
     .version('0.0.1', '-v, --version')
     .name('cli');
@@ -93,11 +122,53 @@ program
     });
 
 program
-    .command('*', {noHelp: true})
-    .action(() => {
-        console.log('해당 명령어를 찾을 수 없습니다.');
-        program.help();
+    .command('copy <name> <directory>')
+    .usage('<name> <directory>')
+    .description('파일을 복사합니다.')
+    .action((name, directory) => {
+        copyFile(name, directory);
+    });
+
+program
+    .command('remove <path>')
+    .usage('<path>')
+    .description('지정한 경로의 하위 파일/폴더를 지웁니다.')
+    .action((path) => {
+        removeFile(path);
     })
 
 program
+    .action((cmd, args) => {
+        if(args) {
+            console.log(chalk.bold.red('해당 명령어를 찾을 수 없습니다.'));
+            program.help();
+        } else {
+            inquirer.prompt([{
+                type: 'list',
+                name: 'type',
+                message: '템플릿 종류를 선택하세요.',
+                choices: ['html', 'express-router'],
+            }, {
+                type: 'input',
+                name: 'name',
+                message: '파일의 이름을 하세요.',
+                default: 'index',
+            }, {
+                type: 'input',
+                name: 'directory',
+                message: '파일을 저장할 경로를 입력하세요.',
+                defalut: '.',
+            }, {
+                type: 'confirm',
+                name: 'confirm',
+                message: '생성하시겠습니까?',
+            }])
+                .then((answer) => {
+                    if(answer.confirm) {
+                        makeTemplate(answer.type, answer.name, answer.directory);
+                        console.log(chalk.bold.red('터미널을 종료합니다.'));
+                    }
+                });
+        }
+    })
     .parse(process.argv);
